f771404d1fb6cbc5b8956a7764c0f484e9da6258
VatsAditya48 <adityavatsa40@gmail.com>
2025-11-24 11:42:38 +0530

    Add UserDAOTest class with placeholder for tests
    Created a placeholder for future unit tests in UserDAOTest.

---
+++ test/java/dao/UserDAOTest.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class UserDAOTest {
+    // Placeholder for unit tests (use JUnit in the future)
+    public static void main(String[] args) {
+        System.out.println("Add unit tests here.");
+    }
+}
