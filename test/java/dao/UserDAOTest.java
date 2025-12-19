3c528f4c7ad2049fac34f9fd9df4a098c38e7019
VatsAditya48 <adityavatsa40@gmail.com>
2025-12-19 20:04:29 +0530

    Delete test directory

--- test/java/dao/UserDAOTest.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public class UserDAOTest {
-    // Placeholder for unit tests (use JUnit in the future)
-    public static void main(String[] args) {
-        System.out.println("Add unit tests here.");
-    }
-}
